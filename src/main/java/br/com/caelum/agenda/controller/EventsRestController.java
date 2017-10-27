1001197bf6122a95f2081b0874eb3a469b12372e
Matheus Brandino <m.v.brandino@gmail.com>
2017-10-27 16:55:13 -0200

    refatorando o nome do controller

--- src/main/java/br/com/caelum/agenda/controller/EventsRestController.java
+++ src/main/java/br/com/caelum/agenda/controller/EventsRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("events")
-public class EventsController {
+public class EventsRestController {
 
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
